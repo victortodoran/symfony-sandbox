09e31d0bc32ab3f28c1063c4f0818b850b1d5cc5
Victor Todoran <victor.todoran@evozon.com>
2022-05-10 14:16:56 +0300

    Add dependencies

---
+++ src/Controller/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'test')]
+    public function test(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
